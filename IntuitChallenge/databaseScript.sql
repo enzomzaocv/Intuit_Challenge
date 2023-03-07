f51511346e2a13c17c21e78129c0a0d4346d403e
Enzo Chacon <enzo_ocv@hotmail.com>
2023-03-06 22:53:17 -0300

    Se agrega script

--- IntuitChallenge/databaseScript.sql
+++ IntuitChallenge/databaseScript.sql
@@ -16,3 +16,42 @@ BEGIN
     ) ON [PRIMARY]
 END
 GO
+
+
+INSERT INTO [Customer]
+    ([Name],
+    [Surname],
+    [Birthdate],
+    [IdentificationNumber],
+    [Adress],
+    [Phone],
+    [Email])
+VALUES
+    ('Pedro',
+    'Aguilar',
+    DATEADD(YEAR, -10, GETDATE()),
+    '20-11111111-8',
+    'San juan 1234, Mendoza',
+    '261-5760564',
+    'pedro@gmail.com'),
+    ('Pablo',
+    'Aguinaga',
+    DATEADD(YEAR, -11, GETDATE()),
+    '20-11118811-8',
+    'San juan 34, Mendoza',
+    '261-5168564',
+    'pablo@gmail.com'),
+    ('Ariana',
+    'Salar',
+    DATEADD(YEAR, -18, GETDATE()),
+    '20-38118811-8',
+    'San Pedro 1234, Mendoza',
+    '261-5192564',
+    'ariana@gmail.com'),
+    ('Samanta',
+    'Camba',
+    DATEADD(YEAR, -22, GETDATE()),
+    '20-39618811-8',
+    'San Martin 1234, Mendoza',
+    '261-5192954',
+    'samanta@gmail.com')
